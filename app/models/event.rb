af8ada80470aedb658af5365d9affa5398f3d026
pjpoole <peter.j.poole@gmail.com>
2015-01-08 20:28:03 -0500

    Posts can be made about events

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,5 @@ class Event < ActiveRecord::Base
   validates :name, :event_type, :date, presence: true
 
   belongs_to :section
+  has_many :posts, dependent: :destroy
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,6 @@ class Post < ActiveRecord::Base
 
   belongs_to :user
   belongs_to :section
+  belongs_to :event 
   has_many :comments, dependent: :destroy
 end
