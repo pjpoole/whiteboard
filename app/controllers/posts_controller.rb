03f6582c1ec88719d41666bd9e4d74d0556ba451
pjpoole <peter.j.poole@gmail.com>
2015-01-09 16:12:52 -0500

    Add Comments controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,7 +27,7 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+    @post = Post.includes(:comments).find(params[:id])
 
     if @post
       render :show
@@ -47,8 +47,6 @@ class PostsController < ApplicationController
       :user_id,
       :event_id
     )
-
-
   end
 
 end
