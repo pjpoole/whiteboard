7a3b7c9cbc1f2e2ad22d0fd59a384af0e849dca4
pjpoole <peter.j.poole@gmail.com>
2015-01-16 11:51:01 -0500

    Add parse to Post

--- app/assets/javascripts/models/post.js
+++ app/assets/javascripts/models/post.js
@@ -1,3 +1,19 @@
 Whiteboard.Models.Post = Backbone.Model.extend({
-  urlRoot: '/api/posts'
+  urlRoot: '/api/posts',
+
+  comments: function () {
+    if (!this._comments) {
+      this._comments = new Whiteboard.Collections.Comments();
+    }
+    return this._comments;
+  },
+
+  parse: function (resp) {
+    if (resp.comments) {
+      this.comments().set(resp.comments, { parse: true })
+      delete resp.comments
+    }
+
+    return resp;
+  }
 });
