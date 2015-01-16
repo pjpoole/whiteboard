45f524c7f88e2c8a7f4feffa7cf5bcd280137af3
pjpoole <peter.j.poole@gmail.com>
2015-01-15 20:51:45 -0500

    Create CurrentUser model

--- app/assets/javascripts/models/user.js
+++ app/assets/javascripts/models/user.js
@@ -1,3 +1,90 @@
 Whiteboard.Models.User = Backbone.Model.extend({
   urlRoot: '/api/users'
 });
+
+Whiteboard.Models.CurrentUser = Whiteboard.Models.User.extend({
+  url: '/api/session',
+
+  parse: function (resp) {
+    if (resp.sections) {
+      this.sections().set(resp.sections, { parse: true });
+      delete resp.sections;
+    }
+
+    if (resp.sectionsInstructed) {
+      this.sectionsInstructed().set(
+        resp.sectionsInstructed, { parse: true }
+      );
+      delete resp.sectionsInstructed;
+    }
+
+    return resp;
+  },
+
+  initialize: function (options) {
+    this.listenTo(this, 'change', this.fireSessionEvent);
+  },
+
+  isSignedIn: function () {
+    return !this.isNew();
+  },
+
+  signIn: function (options) {
+    var model = this;
+
+    var credentials = {
+      'user[email]': options.email,
+      'user[password]': options.password
+    };
+    debugger
+    $.ajax({
+      url: this.url,
+      type: 'POST',
+      data: credentials,
+      dataType: 'json',
+      success: function (data) {
+        model.parse(data);
+        debugger
+        model.set(data);
+        options.success && options.success();
+      },
+      error: function () {
+        options.error && options.error();
+      }
+    });
+  },
+
+  signOut: function (options) {
+    var model = this;
+
+    // TODO: Why can't this be a Backbone method?
+    $.ajax({
+      url: this.url,
+      type: 'DELETE',
+      dataType: 'json',
+      success: function (data) {
+        model.clear();
+        options.success && options.success();
+      }
+    })
+  },
+
+  fireSessionEvent: function () {
+    if (this.isSignedIn()) this.trigger('signIn');
+    else this.trigger('signOut');
+  },
+
+  sections: function () {
+    if (!this._sections) {
+      this._sections = new Whiteboard.Collections.Sections();
+    }
+    return this._sections;
+  },
+
+  sectionsInstructed: function () {
+    if (!this._sectionsInstructed) {
+      this._sectionsInstructed = new Whiteboard.Collections.Sections();
+    }
+    return this._sectionsInstructed;
+  }
+});
