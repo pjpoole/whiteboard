89656e7d82458a1a6057d90d15c37a9e91f52464
Peter Poole <peter.j.poole@gmail.com>
2015-02-03 15:31:06 -0500

    Add relevant parsing and relation code to post model

--- app/assets/javascripts/models/post.js
+++ app/assets/javascripts/models/post.js
@@ -8,6 +8,28 @@ Whiteboard.Models.Post = Backbone.Model.extend({
     return this._comments;
   },
 
+  section: function () {
+    if (!this._section) {
+      this._section = eventChannel.request(
+        'user:section', this.get('section_id')
+      );
+    }
+
+    return this._section;
+  },
+
+  vent: function () {
+    var event_id = this.get('event_id');
+
+    if (event_id && !this._vent) {
+      this._vent = this.section().vents().getOrFetch(event_id);
+    } else {
+      this._vent = undefined;
+    }
+
+    return this._vent;
+  },
+
   parse: function (resp) {
     if (resp.comments) {
       this.comments().set(resp.comments, { parse: true })
