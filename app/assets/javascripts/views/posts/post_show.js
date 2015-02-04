96119bf3ddeaaf8af248041de3bfceee5a8abfdb
Peter Poole <peter.j.poole@gmail.com>
2015-02-04 12:18:44 -0500

    Basic post show view

--- app/assets/javascripts/views/posts/post_show.js
+++ app/assets/javascripts/views/posts/post_show.js
@@ -1,5 +1,16 @@
 Whiteboard.Views.PostShow = Mn.CompositeView.extend({
   template: JST['posts/show'],
+  templateHelpers: function () {
+    var model = this.model;
+    return {
+      sectionId: model.section_id,
+      sectionTitle: model.section().get('title'),
+      hasEvent: !!model.event_id,
+      eventId: model.event_id,
+      eventName: model.vent().get('name'),
+      postTopic: model.escape('topic'),
+      postBody: model.escape('body')
+    }
+  },
 
-  
 });
