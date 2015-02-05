9c00b116a6bf2f2a939cfda685c0dbd9833fb651
Peter Poole <peter.j.poole@gmail.com>
2015-02-04 19:59:16 -0500

    Add model attribute to collection

--- app/assets/javascripts/collections/posts.js
+++ app/assets/javascripts/collections/posts.js
@@ -1,5 +1,6 @@
 Whiteboard.Collections.Posts = Backbone.Collection.extend({
   url: '/api/posts',
+  model: Whiteboard.Models.Post,
 
   initialize: function (models, options) {
     options || (options = {});
--- app/assets/javascripts/collections/sections.js
+++ app/assets/javascripts/collections/sections.js
@@ -1,5 +1,4 @@
 Whiteboard.Collections.Sections = Backbone.Collection.extend({
   url: '/api/sections',
-
   model: Whiteboard.Models.Section
 });
