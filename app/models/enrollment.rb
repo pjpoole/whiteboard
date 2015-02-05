c28a252746f932bb24aa07cc4a686fe57f646e89
Peter Poole <peter.j.poole@gmail.com>
2015-02-04 22:31:12 -0500

    Add tasks to student on enrollment and on event create

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,4 +1,17 @@
 class Enrollment < ActiveRecord::Base
   belongs_to :user
   belongs_to :section
+
+  after_create :add_tasks_to_student
+
+  private
+  def add_tasks_to_student
+    section = Section.includes(:events).find(self.section_id)
+
+    transaction do
+      section.events.each do |event|
+        Task.create(user_id: self.user_id, event_id: event.id)
+      end
+    end
+  end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -13,4 +13,17 @@ class Event < ActiveRecord::Base
   has_many :posts, dependent: :destroy
 
   has_many :tasks, dependent: :destroy
+
+  after_create :create_tasks_for_students
+
+  private
+  def create_tasks_for_students
+    section = Section.includes(:students).find(self.section_id)
+
+    transaction do
+      section.students.each do |student|
+        Task.create(user_id: student.id, event_id: self.id)
+      end
+    end
+  end
 end
