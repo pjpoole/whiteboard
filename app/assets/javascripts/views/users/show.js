f815ef328bb9308c29caf3015ca8e25c3698eb4d
Peter Poole <peter.j.poole@gmail.com>
2015-02-11 20:32:01 -0500

    Show user name on user show page

--- app/assets/javascripts/views/users/show.js
+++ app/assets/javascripts/views/users/show.js
@@ -1,4 +1,18 @@
 Whiteboard.Views.UserShow = Mn.ItemView.extend({
   tagName: 'section',
-  template: JST['users/show']
+  template: JST['users/show'],
+  templateHelpers: function () {
+    var model = this.model;
+
+    return {
+      userName: model.escape('name')
+    }
+  },
+
+  modelEvents: {
+    'sync': 'render'
+  },
+
+  initialize: function () {
+  }
 });
