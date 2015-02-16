480b6e06ee33e16db2ad614f4a61b705998a164b
Peter Poole <peter.j.poole@gmail.com>
2015-02-16 16:02:23 -0500

    Prune rails views and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,6 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'api/session' => 'api/sessions#show'
   end
-  # # Rails root
-  # root to: 'sessions#new'
 
   # Backbone root
   root to: 'pages#root'
@@ -28,37 +26,4 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :destroy]
   end
 
-
-  # # Rails sign in
-  # resources :users, only: [:new, :create]
-  # resource :session, only: [:new, :create, :destroy]
-
-
-  # # Rails
-  #
-  # I deliberately keep these sparse/conservative, only creating routes
-  # for essential actions inasmuch as possible. There are still some
-  # orphaned routes, though, sadly.
-  #
-  # TODO: Clean up orphaned routes.
-  # resources :users do
-  #   resources :enrollments, only: [:index, :create]
-  # end
-  #
-  # resources :sections do
-  #   resources :events, only: [:new, :create, :index]
-  #   resources :posts, only: [:new, :create, :index]
-  # end
-  #
-  # resources :events do
-  #   resources :posts, only: [:new, :create, :index]
-  # end
-  #
-  # resources :posts do
-  #   resources :comments, only: [:new, :create]
-  # end
-  #
-  # resources :comments, only: [:edit, :update, :destroy]
-  #
-  # resources :enrollments, only: [:destroy]
 end
